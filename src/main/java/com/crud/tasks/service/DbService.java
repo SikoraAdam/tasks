11cd7106733584d0a2cc48416d5aa9e72a3deb56
Cross <C%3Rwien>
2018-09-24 12:41:04 +0200

    Module_15.2 v1

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -15,5 +16,13 @@ public class DbService {
 
     public List<Task> getAllTasks() {
         return repository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        Optional<Task> matchingTask = repository.findAll().stream()
+                .filter(task -> task.getId()==id)
+                .findFirst();
+
+        return matchingTask.orElse(null);
     }
 }
