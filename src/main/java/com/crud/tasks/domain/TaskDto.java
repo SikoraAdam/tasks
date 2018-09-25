c21bd0a634d66ba22068c751e3fa076d672966dd
Cross <C%3Rwien>
2018-09-25 09:35:36 +0200

    Module_15.3 v1

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,12 +1,21 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class TaskDto {
+
     private Long id;
     private String title;
     private String content;
+
+    @JsonCreator
+    public TaskDto(@JsonProperty("title") String title, @JsonProperty("content") String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
