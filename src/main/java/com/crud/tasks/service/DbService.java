0f04e1c04ab58ad0aa1548c1977a297f05bfd2a9
Cross <C%3Rwien>
2018-09-26 10:35:36 +0200

    Module_15.3 v2

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,9 +1,11 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +35,7 @@ public class DbService {
         return repository.findById(id);
     }
 
+    @Transactional
     public void deleteTask(final Long id) {
         repository.deleteById(id);
     }
