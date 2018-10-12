9951c7f4e878a5aa03d1be4429af0cd1ab21327c
SikoraAdam <sikora.adam@mail.com>
2018-10-12 14:58:32 +0200

    Module_18.2

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,10 +1,10 @@
 package com.crud.tasks.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configurable
+@Configuration
 public class CoreConfiguration {
 
     @Bean
