c410c936cd00c287382c34466adf26edb4e3bddf
SikoraAdam <sikora.adam@mail.com>
2018-10-12 22:34:41 +0200

    Module_18.3

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -19,10 +19,19 @@ public class TrelloController {
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
     public void getTrelloBoards() {
 
+        // GET request
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+        trelloBoards.forEach(trelloBoardDto -> {
 
+            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+
+            System.out.println("This board contains lists: ");
+
+            trelloBoardDto.getLists().forEach(trelloList ->
+                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+
+        });
         //Module_18.2
         /*trelloBoards.stream().filter(trelloBoardDto -> !trelloBoardDto.getId().isEmpty()
                 && (!trelloBoardDto.getName().isEmpty() || trelloBoardDto.getName().contains("Kodilla")))
