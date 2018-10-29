411312dafe914eddbd4e8656e01b22e7a7db9672
SikoraAdam <sikora.adam@mail.com>
2018-10-29 09:43:22 +0100

    Module_19.4 v0

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -1,4 +1,43 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Optional.ofNullable;
+
+@Service
 public class TrelloService {
+
+    private static final String SUBJECT = "Tasks: New Trello card";
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createTrelloCard (TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+
+        final String tpl = "New card \"%s\" has been created on your Trello account";
+        ofNullable(newCard).ifPresent(
+                card -> emailService.send(new Mail(adminConfig.getAdminMail(), "cc@gmail.com", SUBJECT,
+                        "New card " + card.getName() + " has been created")));
+        return newCard;
+    }
 }
