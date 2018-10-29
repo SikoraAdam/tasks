84f5cb0cf8aa1768d9371d85dffdd4a721a33ee1
SikoraAdam <sikora.adam@mail.com>
2018-10-29 11:01:59 +0100

    Module_20.1 v0

--- src/test/java/com/crud/tasks/trello/mapper/TrelloMapperTest.java
+++ src/test/java/com/crud/tasks/trello/mapper/TrelloMapperTest.java
@@ -2,8 +2,70 @@ package com.crud.tasks.trello.mapper;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrelloMapperTest {
 
+    @Test
+    public void mapToBoardTest(){
 
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    @Test
+    public void mapToBoardTestDto(){
+
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void mapToListTest(){
+
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void mapToListTestDto(){
+
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void mapToCardTest(){
+
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void mapToCardTestDto(){
+
+        //Given
+
+        //When
+
+        //Then
+    }
 
 }
