ce8152a2f72508f88ee4717ba23283728b103daf
SikoraAdam <sikora.adam@mail.com>
2018-10-30 17:29:28 +0100

    Module_19.4 v3

---
+++ src/main/java/com/crud/tasks/sheduler/EmailSchedulerV2.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.sheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class EmailSchedulerV2 {
+
+    private static final String SUBJECT = "Tasks: Once a day email";
+    private static final String CC = "adam.x.sikora@gmail.com";
+    private static final int ONE_TASK = 1;
+    private static final String SINGLE_TASK = "task";
+    private static final String MULTIPLE_TASK = "tasks";
+
+    private SimpleEmailService emailService;
+    private TaskRepository taskRepository;
+    private AdminConfig adminConfig;
+
+    /**
+     *  zakomentowane żeby nie chodziło w nieskończoność
+     */
+    //@Scheduled(fixedDelay = 10000)
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        emailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                CC,
+                SUBJECT,
+                "Currently in database you got: " + size + (size == ONE_TASK ? SINGLE_TASK : MULTIPLE_TASK)));
+    }
+}
